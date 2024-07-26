6bf38ed4af163442731bf253fe91216afd6b50e8
HasancanCakicioglu <hckecommerce@gmail.com>
2024-07-26 18:03:56 +0300

    cors

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://voidnote-frontend.onrender.com/',
 }));
 
 // Start the server
